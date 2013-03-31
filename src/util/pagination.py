93897d665f82d94f762a2530d9929304584ef8e1
Dave Wilson <dave.wilson1@ntlworld.com>
2013-03-31 15:35:53 +0100

    *

--- src/util/pagination.py
+++ src/util/pagination.py
@@ -7,60 +7,61 @@ Created on 14 Feb 2012
 from math import ceil
 from collections import namedtuple
 
-Page = namedtuple('Page', 'pageNo noPages noRecords recordsStart '
+Page = namedtuple('Page', 'pageNo numPages numRecords recordsStart '
                          'recordsEnd details offset limit bottom top')
 
 
-def validatePageNo(pageNo, noPages):
+def validatePageNo(pageNo, numPages):
     try:
         pageNo = int(pageNo)
     except (TypeError, ValueError):
         return 1
     if pageNo < 1:
         return 1
-    elif pageNo > noPages:
-        return noPages
+    elif pageNo > numPages:
+        return numPages
 
     return pageNo
 
 
-def calcNoPages(noRecords, perPage, orphans):
-    if not noRecords:
+def calcNoPages(numRecords, perPage, orphans):
+    if not numRecords:
         return 1
-    hits = max(1, noRecords - orphans)
+    hits = max(1, numRecords - orphans)
     return int(ceil(hits / float(perPage)))
 
 
-def pageDetails(pageNo, noPages, noRecords, recordsStart, recordsEnd):
-    if not noRecords:
+def pageDetails(pageNo, numPages, numRecords, recordsStart, recordsEnd):
+    if not numRecords:
         return 'No Records'
-    elif noPages == 1:
+    elif numPages == 1:
         pages = 'Page 1'
     else:
-        pages = 'Page {} of {}'.format(pageNo, noPages)
+        pages = 'Page {} of {}'.format(pageNo, numPages)
 
-    if noRecords == 1:
+    if numRecords == 1:
         records = '1 Record'
     else:
         records = 'Records {} to {}'.format(recordsStart, recordsEnd)
 
-    if noPages > 1:
-        records = '{} of {} Records'.format(records, noRecords)
+    if numPages > 1:
+        records = '{} of {} Records'.format(records, numRecords)
 
     return '{} - {}'.format(pages, records)
 
 
-def pagination(pageNo, noRecords, perPage=1000, orphans=0):
-    noPages = calcNoPages(noRecords, perPage, orphans)
-    pageNo = validatePageNo(pageNo, noPages)
-    recordsStart = perPage * (pageNo - 1) + 1 if noRecords > 1 else 1
-    recordsEnd = pageNo * perPage if pageNo != noPages else noRecords
+def pagination(pageNo, numRecords, perPage=1000, orphans=0):
+    numPages = calcNoPages(numRecords, perPage, orphans)
+    pageNo = validatePageNo(pageNo, numPages)
+    recordsStart = perPage * (pageNo - 1) + 1 if numRecords > 1 else 1
+    recordsEnd = pageNo * perPage if pageNo != numPages else numRecords
     offset = bottom = recordsStart - 1
     limit = recordsEnd - offset
     top = bottom + limit
-    details = pageDetails(pageNo, noPages, noRecords, recordsStart, recordsEnd)
-    return Page(pageNo, noPages, noRecords, recordsStart, recordsEnd, details,
-                       offset, limit, bottom, top)
+    details = pageDetails(pageNo, numPages, numRecords, recordsStart,
+                          recordsEnd)
+    return Page(pageNo, numPages, numRecords, recordsStart, recordsEnd,
+                details, offset, limit, bottom, top)
 
 if __name__ == '__main__':
     print pagination(2, 3005)
